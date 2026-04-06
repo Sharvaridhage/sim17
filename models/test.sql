d7754272337f3175a601802e28bb750470ad8aa7
Sharvaridhage <146980287+Sharvaridhage@users.noreply.github.com>
2026-04-06 19:26:54 +0530

    Update test.sql

--- models/test.sql
+++ models/test.sql
@@ -5,10 +5,11 @@ SELECT
     department,
     salary,
     joining_date,
+
     CASE 
         WHEN salary < 65000 THEN 'Low'
         WHEN salary BETWEEN 65000 AND 75000 THEN 'Medium'
         ELSE 'High'
     END AS salary_level
 
-FROM employees
+FROM "PIYUSHK_DB"."PUBLIC"."EMPLOYEES"
